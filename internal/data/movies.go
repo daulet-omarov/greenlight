fdfd9d83bad0f81616d23e4355022e41d79431c3
daulet-omarov <dauka.omar04@gmail.com>
2025-05-08 14:51:17 +0500

    returning pagination metadata

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -152,9 +152,9 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
+func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
 	query := fmt.Sprintf(`
-		SELECT id, created_at, title, year, runtime, genres, version
+		SELECT count(*) OVER(), id, created_at, title, year, runtime, genres, version
 		FROM movies
 		WHERE (STRPOS(LOWER(title), LOWER($1)) > 0 OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
@@ -168,16 +168,18 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 
 	rows, err := m.DB.QueryContext(ctx, query, args...)
 	if err != nil {
-		return nil, err
+		return nil, Metadata{}, err
 	}
 	defer rows.Close()
 
+	var totalRecords int
 	var movies = []*Movie{}
 
 	for rows.Next() {
 		var movie Movie
 
 		err = rows.Scan(
+			&totalRecords,
 			&movie.ID,
 			&movie.CreatedAt,
 			&movie.Title,
@@ -188,11 +190,17 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 		)
 
 		if err != nil {
-			return nil, err
+			return nil, Metadata{}, err
 		}
 
 		movies = append(movies, &movie)
 	}
 
-	return movies, nil
+	if err = rows.Err(); err != nil {
+		return nil, Metadata{}, rows.Err()
+	}
+
+	metadata := calculateMetaData(totalRecords, filters.Page, filters.PageSize)
+
+	return movies, metadata, nil
 }
